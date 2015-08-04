b5f5b4bcad164dcebb45da6061d68202a3d3d74b
Terry Marr & Austin Kincaid <student@epicodus.com>
2015-08-04 09:59:45 -0700

    adds method to check whether date is Saturday or Sunday and returns to user ok to sleep in

--- lib/sleep_in.rb
+++ lib/sleep_in.rb
@@ -2,6 +2,15 @@ class String
   define_method(:sleep_in) do
     date_array = self.split("/")
     date = Time.new(date_array[2], date_array[0], date_array[1])
-    return date.strftime('%A')
+
+
+    if date.saturday?() | date.sunday?()
+      return "Go ahead and sleep in, you earned it."
+    else
+      return date.strftime('%A')
+    end
+
   end
 end
+
+# "Go ahead and sleep in, you earned it."
--- spec/sleep_in_spec.rb
+++ spec/sleep_in_spec.rb
@@ -6,6 +6,7 @@ describe('String#sleep_in') do
     expect(('8/4/2015').sleep_in()).to(eq("Tuesday"))
   end
 
-  
-
+  it("tells the user they can sleep in if the date is Saturday or Sunday") do
+    expect(('8/1/2015').sleep_in()).to(eq("Go ahead and sleep in, you earned it."))
+  end
 end
